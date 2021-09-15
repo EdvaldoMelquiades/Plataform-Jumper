86e4a796cae9f4a55c563e191f842a6295ea954a
EdvaldoMelquiades <melquiades.emc@gmail.com>
2021-09-15 20:48:34 -0300

    Initial Commit

---
+++ Assets/Scripts/Game_over.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Game_over : MonoBehaviour {
+
+	public Transform character;
+	int alto = 0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (character.position.y > 5) {
+
+			alto = 1;
+
+		}
+
+		if (alto == 1) {
+
+			SceneManager.LoadScene ("End", LoadSceneMode.Single);
+
+		}
+
+	}
+
+}
